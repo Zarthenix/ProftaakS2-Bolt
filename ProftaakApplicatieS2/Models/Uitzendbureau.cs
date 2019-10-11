128b3c566a7b2452a4fc4648b241f81efcbd6db7
Emre <emre_ftw@live.nl>
2019-10-11 10:14:57 +0200

    Opzet en begin van basis, modellen hebben nu fields en constructors

---
+++ ProftaakApplicatieS2/Models/Uitzendbureau.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProftaakApplicatieS2.Models
+{
+    public class Uitzendbureau
+    {
+        public int ID { get; set; }
+        public string Naam { get; set; }
+        public int Eigenaar { get; set; }
+        public Uitzendbureau()
+        {
+
+        }
+    }
+}
---
+++ ProftaakApplicatieS2/Views/UitzendView.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProftaakApplicatieS2.Views
+{
+    public class UitzendView
+    {
+    }
+}
