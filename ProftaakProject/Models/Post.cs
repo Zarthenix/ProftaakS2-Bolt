84a1be0f5bda6fb37771a7a7f3fb8285361db245
Stan <426638@student.fontys.nl>
2019-11-06 11:06:11 +0100

    partial post toevoeging

--- ProftaakProject/Models/Post.cs
+++ ProftaakProject/Models/Post.cs
@@ -8,9 +8,10 @@ namespace ProftaakProject.Models
     public class Post
     {
         public int Id { get; set; }
+        public Account Auteur { get; set; }
         public int GoedgekeurdDoor { get; set; }
         public int AantalBekenen { get; set; }
-        public string Onderwerp { get; set; }
+        public string Titel { get; set; }
         public DateTime Datum { get; set; }
         public string Inhoud { get; set; }
         public enum Type
