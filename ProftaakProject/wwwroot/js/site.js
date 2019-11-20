f56a2d35b166adb222c70404c6fd26f299e9fb9d
Emre <emre_ftw@live.nl>
2019-11-20 09:49:08 +0100

    navbar is nu collapisble en dunner

--- ProftaakProject/wwwroot/js/site.js
+++ ProftaakProject/wwwroot/js/site.js
@@ -1,4 +1,15 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿$(function() {
+    $("#nav").button().hover(function() {
+        openNav();
+    });
+});
 
-// Write your JavaScript code.
+function openNav() {
+    document.getElementById("mySidebar").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+}
+
+function closeNav() {
+    document.getElementById("mySidebar").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+}
