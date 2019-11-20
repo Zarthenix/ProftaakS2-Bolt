17590d0f6955fdb47fc300955404fe7d360f5762
Linda <linda.franken@student.fontys.nl>
2019-11-20 14:51:00 +0100

    Auto stash before merge of "PartialPost" and "origin/PartialPost"

---
+++ ProftaakProject/Models/ConvertModels/PostToPostvmConverter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ProftaakProject.Models.ViewModels.Post;
+
+namespace ProftaakProject.Models.ConvertModels
+{
+    public class PostToPostvmConverter
+    {
+        public Post ConvertToModel(PostViewModel pvm)
+        {
+            Post p = new Post();
+            {
+                p.Inhoud = pvm.Inhoud;
+                p.Titel = pvm.Titel;
+                p.Datum = pvm.Datum;
+            }
+            return p;
+        }
+        public PostViewModel ConvertToViewModel(Post p)
+        {
+            PostViewModel pvm = new PostViewModel();
+            {
+                pvm.Inhoud = p.Inhoud;
+                pvm.Titel = p.Titel;
+                pvm.Datum = p.Datum;
+            }
+            return pvm;
+        }
+    }
+}
