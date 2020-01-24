173f134c335a69aabefdc06dd7b1d16f66964e34
Emre <emre_ftw@live.nl>
2020-01-24 09:46:20 +0100

    voor een of andere reden werkt de artikel view niet meer bij de anderen?

---
+++ ProftaakTest/AccountTest.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ProftaakProject.Models.ViewModels.AccountModels;
+using System.Collections.Generic;
+using ProftaakProject.Repositories;
+using ProftaakProject.Context.Interfaces;
+using ProftaakProject.Context.TestContext;
+using ProftaakProject.Models;
+
+namespace ProftaakTest
+{
+    [TestClass]
+    public class AccountTests
+    {
+        [TestMethod]
+        public void TestLogin()
+        {
+            //Arrange
+
+            IAccountContext _Iac = new TestAccountContext();
+            //AccountRepo _ar = new AccountRepo(_Iac);
+
+            //Act
+
+            //Assert
+            //Assert.IsTrue();
+        }
+        [TestMethod]
+        public void TestRegister()
+        {
+            //Arrange
+
+            IAccountContext _Iac = new TestAccountContext();
+            //AccountRepo _ar = new AccountRepo(_Iac);
+            //Act
+            //Assert
+            //Assert.IsTrue();
+        }
+    }
+}
